6018119b9aca835817089d7923e4f706f0d7989f
Gunyoung-Kim <gun025bba@naver.com>
2021-08-03 11:37:36 +0900

    테스트 코드 작성: FeedbackController, ManagerExerciseController에 대한 통합 테스트 코드 작성

---
+++ src/test/java/com/gunyoung/tmb/controller/ManagerExerciseControllerTest.java
@@ -0,0 +1,168 @@
+package com.gunyoung.tmb.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gunyoung.tmb.domain.exercise.Exercise;
+import com.gunyoung.tmb.dto.response.ExerciseForTableDTO;
+import com.gunyoung.tmb.enums.TargetType;
+import com.gunyoung.tmb.repos.ExerciseRepository;
+import com.gunyoung.tmb.utils.PageUtil;
+
+/**
+ * {@link ManagerExerciseController} 에 대한 테스트 클래스
+ * 테스트 범위:(통합 테스트) 프레젠테이션 계층 - 서비스 계층 - 영속성 계층 <br>
+ * MockMvc 활용을 통한 통합 테스트
+ * @author kimgun-yeong
+ *
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ManagerExerciseControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ExerciseRepository exerciseRepository;
+	
+	/**
+	 *  --------------- 테스트 진행과정에 있어 필요한 리소스 반환 메소드들 --------------------- 
+	 */
+	
+	private Exercise getExerciseInstance(String name, TargetType target) {
+		Exercise exercise = Exercise.builder()
+				.name(name)
+				.description("description")
+				.caution("caution")
+				.movement("movement")
+				.target(target)
+				.build();
+		
+		return exercise;
+	}
+	
+	private Long getNonExistExerciseId() {
+		Long nonExistExerciseId = Long.valueOf(1);
+		
+		for(Exercise u : exerciseRepository.findAll()) {
+			nonExistExerciseId = Math.max(nonExistExerciseId, u.getId());
+		}
+		nonExistExerciseId++;
+		
+		return nonExistExerciseId;
+	}
+	
+	private Map<String, Object> getResponseModel(MvcResult mvcResult) {
+		return mvcResult.getModelAndView().getModel();
+	}
+	
+	/**
+	 *  ---------------------------- 본 테스트 코드 ---------------------------------
+	 */
+	
+	/*
+	 * @RequestMapping(value="/manager/exercise",method= RequestMethod.GET)
+	 * public ModelAndView exerciseViewForManager(@RequestParam(value="page" , required=false,defaultValue="1") Integer page,@RequestParam(value="keyword",required=false) String keyword,ModelAndView mav)
+	 */
+	
+	@WithMockUser(roles= {"MANAGER"})
+	@Test
+	@Transactional
+	@DisplayName("Exercise 리스트 화면 반환 -> 정상")
+	public void exerciseViewForManagerTest() throws Exception {
+		//Given
+		int exerciseNum = 10;
+		List<Exercise> exerciseList = new LinkedList<>();
+		
+		for(int i=1;i<=exerciseNum; i++) {
+			Exercise exercise = getExerciseInstance("exericse" + i , TargetType.ARM);
+			exerciseList.add(exercise);
+		}
+		
+		exerciseRepository.saveAll(exerciseList);
+		
+		//When
+		MvcResult result = mockMvc.perform(get("/manager/exercise"))
+		
+		//Then
+				.andExpect(status().isOk())
+				.andReturn();
+		Map<String, Object> model = getResponseModel(result);
+		
+		@SuppressWarnings("unchecked")
+		List<ExerciseForTableDTO> listObject = (List<ExerciseForTableDTO>) model.get("listObject");
+		
+		assertEquals(Math.min(PageUtil.EXERCISE_INFO_TABLE_PAGE_SIZE, exerciseNum),listObject.size());
+	}
+	
+	/*
+	 * @RequestMapping(value="/manager/exercise/add",method = RequestMethod.GET)
+	 * public ModelAndView addExerciseView(ModelAndView mav)
+	 */
+	
+	@WithMockUser(roles= {"MANAGER"})
+	@Test
+	@Transactional
+	@DisplayName("Exercise 추가 화면 반환 -> 정상")
+	public void addExerciseViewTest() throws Exception {
+		//Given
+		
+		//When
+		mockMvc.perform(get("/manager/exercise/add"))
+		
+		//Then
+				.andExpect(status().isOk());
+	}
+	
+	/*
+	 * @RequestMapping(value="/manager/exercise/modify/{exerciseId}", method = RequestMethod.GET) 
+	 * public ModelAndView modifyExerciseView(@PathVariable("exerciseId") Long exerciseId, ModelAndView mav)
+	 */
+	
+	@WithMockUser(roles= {"MANAGER"})
+	@Test
+	@Transactional
+	@DisplayName("Exercise 수정 화면 반환 -> 해당 ID의 Exercise 없을 때")
+	public void modifyExerciseViewNonExist() throws Exception {
+		//Given
+		Long nonExistExerciseId = getNonExistExerciseId();
+		
+		//When
+		mockMvc.perform(get("/manager/exercise/modify/" + nonExistExerciseId))
+		
+		//Then
+				.andExpect(status().isNoContent());
+	}
+	
+	@WithMockUser(roles= {"MANAGER"})
+	@Test
+	@Transactional
+	@DisplayName("Exercise 수정 화면 반환 -> 정상")
+	public void modifyExerciseViewTest() throws Exception {
+		//Given
+		Exercise exercise = getExerciseInstance("exercise",TargetType.ARM);
+		exerciseRepository.save(exercise);
+		
+		//When
+		mockMvc.perform(get("/manager/exercise/modify/" + exercise.getId()))
+		
+		//Then
+				.andExpect(status().isOk());
+	}
+}
