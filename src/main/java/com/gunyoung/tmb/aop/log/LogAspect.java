81b156a1066a0da43bc05017c66f5f7b790c84fe
Gunyoung-Kim <gun025bba@naver.com>
2021-09-06 19:46:29 +0900

    리팩토링: Logger 빈 등록, 기존의 LogAspect 클래스 속 static 변수 삭제

--- src/main/java/com/gunyoung/tmb/aop/log/LogAspect.java
+++ src/main/java/com/gunyoung/tmb/aop/log/LogAspect.java
@@ -8,12 +8,13 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.gunyoung.tmb.utils.HttpRequestUtil;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * 로깅을 위한 Aspect 클래스 
@@ -22,9 +23,10 @@ import com.gunyoung.tmb.utils.HttpRequestUtil;
  */
 @Component
 @Aspect
+@RequiredArgsConstructor
 public class LogAspect {
 	
-	private static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
+	private final Logger logger;
 	
 	/**
 	 * 컨트롤러에 Request 가 들어오면 Request 메소드,uri, parameters, remote address, 처리 시간 들을 로깅 하기 위한 어드바이스
@@ -50,15 +52,8 @@ public class LogAspect {
 		}
 	}
 	
-	/**
-	 * Request Parameter 들을 로깅을 위해 한줄로 변환하는 메소드 
-	 * @param paramMap
-	 * @return Request Parameter 한줄
-	 * @author kimgun-yeong
-	 */
 	private String paramMapToString(Map<String, String[]> paramMap) {
 		StringBuilder sb = new StringBuilder();
-		
 		paramMap.entrySet().stream().forEach(entry -> {
 			sb.append(entry.getKey() + " : ");
 			for(int i=0 ; i < entry.getValue().length ; i++) {
@@ -66,7 +61,6 @@ public class LogAspect {
 			}
 			sb.append(", ");
 		});
-		
 		String paramString = sb.toString();
 		paramString = paramString.substring(0, paramString.length()-2);
 		
