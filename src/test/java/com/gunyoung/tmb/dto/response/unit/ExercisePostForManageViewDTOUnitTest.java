50f9fde435ee1c4378f35fe3bd7260a10644f096
Gunyoung-Kim <gun025bba@naver.com>
2021-09-07 13:54:01 +0900

    테스트 코드 추가: com.gunyoung.tmb.dto.response 클래스에 대한 단위 테스트 코드 추가

--- src/test/java/com/gunyoung/tmb/dto/response/unit/ExercisePostForManageViewDTOUnitTest.java
+++ src/test/java/com/gunyoung/tmb/dto/response/unit/ExercisePostForManageViewDTOUnitTest.java
@@ -1,5 +1,52 @@
 package com.gunyoung.tmb.dto.response.unit;
 
-public class ExercisePostForManageViewDTOUnitTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.gunyoung.tmb.domain.exercise.ExercisePost;
+import com.gunyoung.tmb.domain.user.User;
+import com.gunyoung.tmb.dto.response.ExercisePostForManageViewDTO;
+import com.gunyoung.tmb.util.ExercisePostTest;
+import com.gunyoung.tmb.util.UserTest;
+
+/**
+ * {@link ExercisePostForManageViewDTO} 에 대한 테스트 클래스 <br>
+ * 테스트 범위: (단위 테스트) ExercisePostForManageViewDTO only
+ * @author kimgun-yeong
+ *
+ */
+public class ExercisePostForManageViewDTOUnitTest {
+	
+	/*
+	 * public static List<ExercisePostForManageViewDTO> of(Iterable<ExercisePost> exercisePosts, User user) 
+	 */
+	
+	@Test
+	@DisplayName("ExercisePost 컬렉션과 User를 통해 ExercisePostForManageViewDTO 리스트 반환 -> 정상")
+	public void ofListAndUserTest() {
+		//Given
+		int givenExercisePostNum = 15;
+		Iterable<ExercisePost> exercisePosts =  getExercisePosts(givenExercisePostNum);
+		User user = UserTest.getUserInstance();
+		
+		//When
+		List<ExercisePostForManageViewDTO> result = ExercisePostForManageViewDTO.of(exercisePosts, user);
+		
+		//Then
+		assertEquals(givenExercisePostNum, result.size());
+	}
+	
+	private Iterable<ExercisePost> getExercisePosts(int exercisePostNum) {
+		List<ExercisePost> exercisePosts = new ArrayList<>();
+		for(int i=0;i<exercisePostNum;i++) {
+			ExercisePost ep = ExercisePostTest.getExercisePostInstance();
+			exercisePosts.add(ep);
+		}
+		return exercisePosts;
+	}
 }
