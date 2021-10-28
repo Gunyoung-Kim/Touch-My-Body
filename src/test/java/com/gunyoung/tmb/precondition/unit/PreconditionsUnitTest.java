2628a787889451fcf7c855ab9b6dbce1f2865c49
Gunyoung-Kim <gun025bba@naver.com>
2021-10-28 14:16:09 +0900

    메소드 추가: Preconditions 클래스에 특정 값보다 작거나 큰지 확인하는 메소드 추가(비교 by Comparable.compareTo)

--- src/test/java/com/gunyoung/tmb/precondition/unit/PreconditionsUnitTest.java
+++ src/test/java/com/gunyoung/tmb/precondition/unit/PreconditionsUnitTest.java
@@ -1,5 +1,6 @@
 package com.gunyoung.tmb.precondition.unit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.lang.reflect.Constructor;
@@ -19,7 +20,7 @@ class PreconditionsUnitTest {
 	
 	@Test
 	@DisplayName("생성자 호출 -> 에러 발생")
-	void constructorTest() throws NoSuchMethodException{
+	void constructorTest() throws NoSuchMethodException {
 		//Given
 		Constructor<Preconditions> constructor = Preconditions.class.getDeclaredConstructor();
 		constructor.setAccessible(true);
@@ -40,9 +41,89 @@ class PreconditionsUnitTest {
 		//Given
 		String messageForException = "Should be nonNull";
 		
-		//When
+		//When, Then
 		assertThrows(PreconditionViolationException.class, () -> {
 			Preconditions.notNull(null, messageForException);
 		});
+	}
+	
+	@Test
+	@DisplayName("Object null 확인 -> notnull") 
+	void notNullTest() {
+		//Given
+		Long object = Long.valueOf(28);
+		String messageForException = "Should be nonNull";
+		
+		//When
+		Long result = Preconditions.notNull(object, messageForException);
+		
+		//Then
+		assertEquals(object, result);
+	}
+	
+	/*
+	 * public static <T extends Comparable<T>> T greaterThan(T object, T another, String message)
+	 */
+	
+	@Test
+	@DisplayName("object가 another보다 큰지 확인 -> 크지 않음")
+	void greaterThanTestNotGreater() {
+		//Given
+		Integer object = Integer.valueOf(24);
+		Integer another = Integer.valueOf(100);
+		String messageForException = "object is not greater than antoher";
+		
+		//When, Then
+		assertThrows(PreconditionViolationException.class, () -> {
+			Preconditions.greaterThan(object, another, messageForException);
+		});
+	}
+	
+	@Test
+	@DisplayName("object가 another보다 큰지 확인 -> 큼")
+	void greaterThanTestGreater() {
+		//Given
+		Integer object = Integer.valueOf(82);
+		Integer another = Integer.valueOf(18);
+		String messageForException = "object is not greater than antoher";
+		
+		//When
+		Integer result = Preconditions.greaterThan(object, another, messageForException);
+		
+		//Then
+		assertEquals(object, result);
+	}
+	
+	/*
+	 * public static <T extends Comparable<T>> T lessThan(T object, T another, String message)
+	 */
+	
+	@Test
+	@DisplayName("object가 another보다 작은지 확인 -> 작지 않음")
+	void lessThanTestNotLess() {
+		//Given
+		Integer object = Integer.valueOf(72);
+		Integer another = Integer.valueOf(66);
+		String messageForException = "object is not less than antoher";
+		
+		//When, Then 
+		assertThrows(PreconditionViolationException.class, () -> {
+			Preconditions.lessThan(object, another, messageForException);
+		});
+	}
+	
+	@Test
+	@DisplayName("object가 another보다 작은지 확인 -> 작음")
+	void lessThanTestLess() {
+		//Given
+		Integer object = Integer.valueOf(10);
+		Integer another = Integer.valueOf(30);
+		String messageForException = "object is not less than antoher";
+		
+		//When
+		Integer result = Preconditions.lessThan(object, another, messageForException);
+		
+		//Then
+		assertEquals(object, result);
 	}
 }
